141dd66e96dd2b16ff52f181b33d24b2a1154cfd
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2021-04-05 09:33:19 -0400

    Trabajando en el contenido

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,48 @@
 import Main from 'components/Layout'
+import { categorias } from '../profiles'
 
 const index = () => {
   return (
     <Main>
-      <h1 className="display-1">Sistema Integrado de Gestion</h1>
+      <h1 className="py-3 text-center">Bienvenido al portal de Sistema Integrado de Gestión de la Empresa de Aprovechamiento Hidráulicos Cienfuegos</h1>
+      <div className="row justify-center">
+        {categorias.map((categoria, key) => {
+          return (
+            <div className="col-3 p-2 m-2 card ">
+              <h3 key={key}>{categoria.nombre}</h3>
+            </div>
+          )
+        })}
+      </div>
+      
+      <style jsx>
+        {`
+          .grid {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-wrap: wrap;
+            max-width: 800px;
+            margin-top: 3rem;
+          }
+
+          .card:hover,
+          .card:focus,
+          .card:active {
+            color: #0070f3;
+            border-color: #0070f3;
+          }
+          .card h3 {
+            margin: 0 0 1rem 0;
+            font-size: 1.5rem;
+          }
+          .card p {
+            margin: 0;
+            font-size: 1.25rem;
+            line-height: 1.5;
+          }
+        `}
+      </style>
     </Main>
   )
 }
---
+++ src/profiles.js
@@ -0,0 +1,57 @@
+export const categorias=[
+    {
+        nombre: 'Carpeta Legal',
+        direccion: '/',
+    },    {
+        nombre: 'Misión y visión',
+        direccion: '/',
+    },    {
+        nombre: 'Organigrama',
+        direccion: '/',
+    },    {
+        nombre: 'Estrategia Empresarial',
+        direccion: '/',
+    },    {
+        nombre: 'Manual de Funcionamiento Interno',
+        direccion: '/',
+    },    {
+        nombre: 'Manual de SIG',
+        direccion: '/',
+    },    {
+        nombre: 'Planificación',
+        direccion: '/',
+    },    {
+        nombre: 'Objetivo de Trabajo',
+        direccion: '/',
+    },    {
+        nombre: 'Información documental Interna',
+        direccion: '/',
+    },    {
+        nombre: 'Requisitos Legales',
+        direccion: '/',
+    },    {
+        nombre: 'Otros Requisitos Aplicables',
+        direccion: '/',
+    },    {
+        nombre: 'Gestión de Riesgos',
+        direccion: '/',
+    },    {
+        nombre: 'Información y Comunicación',
+        direccion: '/',
+    },    {
+        nombre: 'Acciones de Control',
+        direccion: '/',
+    },    {
+        nombre: 'Informes de Revisión',
+        direccion: '/',
+    },    {
+        nombre: 'Gestión de innovación',
+        direccion: '/',
+    },    {
+        nombre: 'Mural',
+        direccion: '/',
+    },    {
+        nombre: 'Boletín Informativo',
+        direccion: '/',
+    },
+]
