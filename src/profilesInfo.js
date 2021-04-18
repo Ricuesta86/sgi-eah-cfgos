3f354e4343c7d2bdfb6bcce815ffc0b71ca2e176
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2021-04-18 12:08:14 -0400

    Se crea el arreglo a mostrar la seccion informacion

---
+++ src/profilesInfo.js
@@ -0,0 +1,57 @@
+    // {
+    //     "id":"",
+    //     "estado":"",
+    //     "informacion":"",
+    //     "descripcion":"",
+    //     "fecha":"",
+    // },
+
+//  estados : ["derogado", "actualizado",]
+
+export const informacion=[
+
+
+    {
+        "id":1,
+        "estado":"actualizado",
+        "informacion":"Procedimiento #7",
+        "descripcion":"Se actualiza el procedimiento #7",
+        "fecha":"15/04/2021",
+    },    {
+        "id":2,
+        "estado":"derogado",
+        "informacion":"Procedimiento #10 por #40",
+        "descripcion":"Se derogado el procedimiento #10",
+        "fecha":"15/04/2021",
+    },    {
+        "id":3,
+        "estado":"actualizado",
+        "informacion":"Procedimiento #15",
+        "descripcion":"Se actualiza el procedimiento #15",
+        "fecha":"15/04/2021",
+    },    {
+        "id":4,
+        "estado":"actualizado",
+        "informacion":"Procedimiento #32",
+        "descripcion":"Se actualiza el procedimiento #32",
+        "fecha":"15/04/2021",
+    },    {
+        "id":5,
+        "estado":"derogado",
+        "informacion":"Procedimiento #35 por el #60",
+        "descripcion":"Se derogado el procedimiento #35",
+        "fecha":"15/04/2021",
+    },    {
+        "id":6,
+        "estado":"actualizado",
+        "informacion":"Procedimiento #70",
+        "descripcion":"Se actualiza el procedimiento #70",
+        "fecha":"15/04/2021",
+    },    {
+        "id":7,
+        "estado":"derogado",
+        "informacion":"Procedimiento #40",
+        "descripcion":"Se derogado el procedimiento #40",
+        "fecha":"15/04/2021",
+    },
+]
