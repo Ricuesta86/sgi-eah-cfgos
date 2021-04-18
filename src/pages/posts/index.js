6dde140f8388863baf71931a9fed5e313c93f86d
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2021-04-18 11:24:09 -0400

    Add link para ir a la vista principal

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -5,20 +5,31 @@ import { categorias } from '../../profiles'
 const Posts = () => {
   return (
     <Principal>
+      <div className="text-right block">
+        <Link href={'/'}>
+          <a>Regresar</a>
+        </Link>
+      </div>
       {categorias.map((categoria, key) => {
         return (
           <div className="p-2" key={key}>
-                <h3 >{categoria.nombre}</h3>
-                <p>{categoria.descripcion}
-            <Link href={`/posts/${categoria.id}`}>
-              <a>
-                <span> Leer mas ...</span>
-              </a>
-            </Link>
+            <h3>{categoria.nombre}</h3>
+            <p>
+              {categoria.descripcion}
+              <Link href={`/posts/${categoria.id}`}>
+                <a>
+                  <span> Leer mas ...</span>
+                </a>
+              </Link>
             </p>
           </div>
         )
       })}
+      <div className="text-right block">
+        <Link href={'/'}>
+          <a>Regresar</a>
+        </Link>
+      </div>
     </Principal>
   )
 }
